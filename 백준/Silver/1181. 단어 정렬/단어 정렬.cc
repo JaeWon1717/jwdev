923045636a9d41413aaf9824711af8d4d90c8882
박재원 <81970136+JaeWon1717@users.noreply.github.com>
2023-05-09 09:17:41 +0900

    [Silver V] Title: 단어 정렬, Time: 84 ms, Memory: 4104 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+string word[20000];
+int cmp(string a, string b)
+{
+	if (a.length() == b.length())	
+		return a < b;
+	
+	else
+		return a.length() < b.length();
+}
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> word[i];
+	}
+	sort(word, word + n, cmp);
+	for (int i = 0; i < n; i++)
+	{
+		if (word[i] == word[i - 1])
+			continue;
+		cout << word[i]<<"\n";
+	}
+}
+
+
