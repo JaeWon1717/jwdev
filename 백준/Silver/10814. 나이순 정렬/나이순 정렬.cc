ba24d0d028caedb65a64a32db34601376585058f
박재원 <81970136+JaeWon1717@users.noreply.github.com>
2023-05-10 14:13:59 +0900

    [Silver V] Title: 나이순 정렬, Time: 96 ms, Memory: 7896 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <functional>
+#include <algorithm>
+#include<string>
+#include<vector>
+using namespace std;
+/*
+* 문제
+온라인 저지에 가입한 사람들의 나이와 이름이 가입한 순서대로 주어진다. 
+이때, 회원들을 나이가 증가하는 순으로, 나이가 같으면 먼저 가입한 사람이 앞에 오는 순서로 정렬하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 온라인 저지 회원의 수 N이 주어진다. (1 ≤ N ≤ 100,000)
+
+둘째 줄부터 N개의 줄에는 각 회원의 나이와 이름이 공백으로 구분되어 주어진다.
+나이는 1보다 크거나 같으며, 200보다 작거나 같은 정수이고, 이름은 알파벳 대소문자로 이루어져 있고, 
+길이가 100보다 작거나 같은 문자열이다. 입력은 가입한 순서로 주어진다.
+
+출력
+첫째 줄부터 총 N개의 줄에 걸쳐 온라인 저지 회원을 나이 순, 
+나이가 같으면 가입한 순으로 한 줄에 한 명씩 나이와 이름을 공백으로 구분해 출력한다.
+*/
+struct member
+{
+    int age;
+    string name;
+};
+bool cmp(const member& x, const member& y)
+{
+    return x.age < y.age;//y의 나이가더크게 정렬 
+}
+int main() {
+    int n;
+    cin >> n;
+    vector<member> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i].age >> v[i].name;
+    }
+    stable_sort(v.begin(), v.end(), cmp);
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i].age << " " << v[i].name<<"\n";
+    }
+  
+   
+    return 0;
+}
