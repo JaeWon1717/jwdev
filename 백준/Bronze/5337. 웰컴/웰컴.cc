0cf0603cb878aae93b5f4f81e4d74b5a9e2f76fd
박재원 <81970136+JaeWon1717@users.noreply.github.com>
2023-05-20 16:13:45 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main()
+{
+	
+	cout << ".  .   ." << "\n" << "|  | _ | _. _ ._ _  _" << "\n" << "|/\\|(/.|(_.(_)[ | )(/.";
+}
